e06f28e9f407afd6b0fc33cb4251948e1bb23465
Anwesh43 <anweshthecool0@gmail.com>
2017-04-27 13:04:35 +0530

    Adding VerticalGallery demo

---
+++ verticalgallery/src/main/java/com/anwesome/ui/verticalgallery/VerticalGallery.java
@@ -0,0 +1,16 @@
+package com.anwesome.ui.verticalgallery;
+
+import android.app.Activity;
+
+/**
+ * Created by anweshmishra on 27/04/17.
+ */
+public class VerticalGallery {
+    private Activity activity;
+    public VerticalGallery(Activity activity) {
+        this.activity = activity;
+    }
+    public void show() {
+
+    }
+}
