d5c533c19568a776b826f1ea05f07a5e8e7b7d8b
Anwesh43 <anweshthecool0@gmail.com>
2017-04-27 14:43:52 +0530

    Adding Indicator

---
+++ verticalgallery/src/main/java/com/anwesome/ui/verticalgallery/Indicator.java
@@ -0,0 +1,31 @@
+package com.anwesome.ui.verticalgallery;
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+
+/**
+ * Created by anweshmishra on 27/04/17.
+ */
+public class Indicator {
+    private float x,y,r;
+    private boolean selected = false;
+    public void setSelected(boolean selected) {
+        this.selected = selected;
+    }
+
+    public Indicator(float x,float y,float r) {
+        this.x = x;
+        this.y = y;
+        this.r = r;
+    }
+    public void draw(Canvas canvas, Paint paint) {
+        if(!selected) {
+            paint.setColor(Color.parseColor("#E0E0E0"));
+        }
+        else {
+            paint.setColor(Color.WHITE);
+        }
+        canvas.drawCircle(x,y,r,paint);
+    }
+}
